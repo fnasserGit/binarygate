081435a2945fd2e9706ae1c2e1fce9c6190bf9b3
Fadi Nasser <33449561+fnasserGit@users.noreply.github.com>
2026-03-04 01:31:14 +0200

    Export fetch for Workers runtime

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,11 @@ type Env = {
   CONTAINER_DO: DurableObjectNamespace;
 };
 
-export class BinarygateContainer extends DurableObject {}
+export class BinarygateContainer extends DurableObject {
+  async fetch(request: Request): Promise<Response> {
+    return new Response("Container is running", { status: 200 });
+  }
+}
 
 export async function fetch(request: Request, env: Env): Promise<Response> {
   const id = env.CONTAINER_DO.idFromName("primary");
